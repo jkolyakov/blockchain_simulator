433b58e55bdd5e4c365438187904dc933b69892e
Siddharth <sidagarwal6991@gmail.com>
2025-03-15 20:32:45 -0400

    Modified logic to add received block to the chain vs adding a self-generated block to the chain

--- blockchain_simulator/block.py
+++ blockchain_simulator/block.py
@@ -15,6 +15,7 @@ class BlockBase(ABC):
         self.parent: 'BlockBase' = parent  # Supports DAG & Chain
         self.miner_id: int = miner_id
         self.children: List['BlockBase'] = []
+        self.tree_weight = self.weight
         self.timestamp: float = timestamp  # Block creation time
         self.weight = 1  # Default weight
 
@@ -36,12 +37,10 @@ class GhostBlock(BlockBase):
     
     def __init__(self, block_id: int, parent: Optional['BlockBase'], miner_id: int, timestamp: float):
         super().__init__(block_id, parent, miner_id, timestamp)
-        self.weight: int = 1  # Default weight
-        self.tree_weight = self.weight
 
     def update_weight(self) -> None:
         """Updates weight based on number of children."""
-        self.tree_weight = self.weight + sum(child.weight for child in self.children)
+        self.tree_weight = self.weight + sum(child.tree_weight for child in self.children)
 
     def add_child(self, block):
         super().add_child(block)
