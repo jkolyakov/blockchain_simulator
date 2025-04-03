6f6814f3c57211ea8b90d553d7935c9c642c3917
viet <viet@cs.toronto.edu>
2025-04-03 16:34:28 -0400

    generic message

--- blockchain_simulator/__init__.py
+++ blockchain_simulator/__init__.py
@@ -5,3 +5,18 @@ from blockchain_simulator.simulator import BlockchainSimulator
 from blockchain_simulator.consensus import GHOSTProtocol
 from blockchain_simulator.network_topology import SimpleRandomTopology, StarTopology, FullyConnectedTopology, RingTopology
 from blockchain_simulator.broadcast import GossipProtocol
+
+
+from importlib.metadata import version, PackageNotFoundError
+from blockchain_simulator._metadata import (
+    __author__,
+    __email__,
+    __license__,
+    __url__,
+    __description__,
+)
+
+try:
+    __version__ = version("blockchain_simulator")  
+except PackageNotFoundError:
+    __version__ = "unknown"
